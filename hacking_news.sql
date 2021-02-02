d23db2df965fb76c364e7eeb866880301e9253d6
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-02 15:07:11 +0100

    initial

---
+++ hacking_news.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `users`(
+  `user_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `user_name` TEXT
+);
+
+CREATE TABLE `news`(
+  `news_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `url_news` TEXT
+);
+
+CREATE TABLE `comments`(
+  `comment_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `news_id` INTEGER,
+  `user_id` INTEGER,
+  `body_comment` TEXT, 
+  FOREIGN KEY (news_id) REFERENCES news(news_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
