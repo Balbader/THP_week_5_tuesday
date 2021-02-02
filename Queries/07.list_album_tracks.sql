11121749cb6377d3f4e983a18fe49466b3af93f6
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-02 16:02:50 +0100

    update

--- Queries/07.list_album_tracks.sql
+++ Queries/07.list_album_tracks.sql
@@ -1,4 +1,1 @@
-SELECT tracks.name 
-FROM tracks
-JOIN albums ON tracks.albumid = albums.albumid
-WHERE albums.title like 'Let There Be Rock';
+SELECT tracks.name FROM tracks JOIN albums ON tracks.albumid = albums.albumid WHERE albums.title like 'Let There Be Rock';
