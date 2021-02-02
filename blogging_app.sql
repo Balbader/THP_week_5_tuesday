3f246fe3cc86a4b8c13af47b7d35f9327d6e5b8c
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-02 13:56:26 +0100

    initial

---
+++ blogging_app.sql
@@ -0,0 +1,41 @@
+CREATE TABLE `users`(
+    `user_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT
+);
+
+CREATE TABLE `articles`(
+    `article_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `content` TEXT,
+    `user_id` INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE `category`(
+    `category_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `title` TEXT,
+);
+
+CREATE TABLE `articles_categories`(
+    `category_id` INTEGER,
+    `article_id` INTEGER,
+    FOREIGN KEY(catgory_id) REFERENCES category(category_id)
+    FOREIGN KEY(article_id) REFERENCES articles(article_id)
+);
+
+CREATE TABLE `tag`(
+    `tag_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `title` TEXT,
+    `color` TEXT,
+);
+
+INSERT INTO users (name) VALUES ("Dentist");
+INSERT INTO articles (content, user_id) VALUES ("J'adore les dents.", 1);
+INSERT INTO category (title) VALUES ("TEETH");
+INSERT INTO tag (title, color) VALUES ("POO", "green");
+
+
+
+
+
+
+
